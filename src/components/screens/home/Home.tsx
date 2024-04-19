5a6e3067678f39a3a3d76757f3ca0464b30c5d58
Nikita <vivat404@mail.ru>
2024-04-19 03:37:05 +0300

    scss

--- src/components/screens/home/Home.tsx
+++ src/components/screens/home/Home.tsx
@@ -1,8 +1,25 @@
+import styles from './Home.module.scss'
+
 function Home(){
     return(
-        <div>
-            <h1>NACHALO</h1>
-        </div>
+            <nav className={styles.navbar}>
+                <div className={styles.container}>
+                    <a href='#' className={styles.navbarBrand}>Farm Product</a>
+
+                    <div className={styles.navbarWrap}>
+                        <ul className={styles.navbarMenu}>
+                            <li>Поставщики</li>
+                            <li>Доставка</li>
+                            <li>Рецепты</li>
+                            <li>Новинки</li>
+                            <li>%Акции</li>
+                            <li>Подарки и сертификаты</li>
+                        </ul>
+                    </div>
+
+                </div>
+               
+            </nav>
     )
 }
 
