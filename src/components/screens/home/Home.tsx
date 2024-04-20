6616eb2e5a3e5cf1de33dce6336be207d9e19ca6
Nikita <vivat404@mail.ru>
2024-04-20 20:10:50 +0300

    fix svg

--- src/components/screens/home/Home.tsx
+++ src/components/screens/home/Home.tsx
@@ -2,24 +2,34 @@ import styles from './Home.module.scss'
 
 function Home(){
     return(
-            <nav className={styles.navbar}>
+            <div className={styles.navbar}>
                 <div className={styles.container}>
-                    <a href='#' className={styles.navbarBrand}>Farm Product</a>
-
+                    <div>
+                        <a href='#' className={styles.navbarBrand}>Farm Product</a>
+                    </div>
                     <div className={styles.navbarWrap}>
                         <ul className={styles.navbarMenu}>
-                            <li>Поставщики</li>
-                            <li>Доставка</li>
-                            <li>Рецепты</li>
-                            <li>Новинки</li>
-                            <li>%Акции</li>
-                            <li>Подарки и сертификаты</li>
+                            <li><a href="" className={styles.submenuLink}>О нас</a>
+                                <ul className={styles.submenu}>
+                                    <li><a href="">Производство</a></li>
+                                    <li><a href="">Магазин</a></li>
+                                    <li><a href="">Сервис</a></li>
+                                </ul>
+                            </li>
+                            <li><a href='#'>Поставщики</a> </li>
+                            <li><a href='#'>Доставка</a></li>
+                            <li><a href='#'>Рецепты</a></li>
+                            <li><a href='#'>Новинки</a></li>
+                            <li><a href='#'>%Акции</a></li>
+                            <li><a href='#'>Подарки и сертификаты</a></li>
                         </ul>
                     </div>
+                </div>
+                <div>
 
                 </div>
                
-            </nav>
+            </div>
     )
 }
 
