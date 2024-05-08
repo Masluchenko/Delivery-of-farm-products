dab78e690d206c91de10a5ac79a72adb5e613be3
Nikita <vivat404@mail.ru>
2024-05-08 06:13:25 +0300

    fix

--- src/components/screens/home/Cards/Cards.tsx
+++ src/components/screens/home/Cards/Cards.tsx
@@ -1,33 +1,36 @@
 import React from 'react'
 import { range } from './assortment.ts'
-import { Grid, Card, CardContent, Typography, CardActionArea} from '@mui/material/';
+import { Grid, Card, CardContent, Typography, CardActionArea, Box} from '@mui/material/';
 
 
 const Cards: React.FC = () => {
 
     return (
+      <Box >
         <Grid sx={{ flexGrow: 1 }} container justifyContent="center">
-          <Grid item xs={7}>
-            <Grid container justifyContent="center" spacing={2}>
+          <Grid item xs={8}>
+            <Typography variant="h4" >Наш ассортимент</Typography>
+            <Grid container justifyContent="center" spacing={2.5}>
               {range.map((range, index) => (
                 <Grid key={index} item>
-                   <CardActionArea>
+                  <CardActionArea>
                     <Card style={{ backgroundImage: `url(${range.image})`,
                       backgroundSize: 'cover',
-                      width: 200,
-                      height: '200px',
+                      width: 270,
+                      height: 270,
                       color: 'white',
                       borderRadius: 16 }}>
                       <CardContent>
                       <Typography variant="h6">{range.name}</Typography>
                       </CardContent>
-                  </Card>
+                    </Card>
                   </CardActionArea>
                 </Grid> 
               ))}
             </Grid>
           </Grid>
        </Grid>
+       </Box>
     );
   };
   
