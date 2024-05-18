4951bc60976232972cdb7724c0cff3cc77996aa9
Nikita <vivat404@mail.ru>
2024-05-18 06:34:05 +0300

    fix

--- src/components/screens/NavElement/Basket/Basket.tsx
+++ src/components/screens/NavElement/Basket/Basket.tsx
@@ -1,9 +1,13 @@
-import { Dialog, List, Typography, Box, Chip, Divider, IconButton } from '@mui/material';
+import { DialogContentText, Typography, Box, Chip, Divider, IconButton, Grid, Paper, Card, CardActionArea, CardContent  } from '@mui/material';
 import ShoppingBasket from '@mui/icons-material/ShoppingBasket';
 import ProductionQuantityLimitsIcon from '@mui/icons-material/ProductionQuantityLimits';
 import React from 'react';
-
-
+import CloseIcon from '@mui/icons-material/Close';
+import { experimentalStyled as styled } from '@mui/material/styles';
+import Dialog, { DialogProps } from '@mui/material/Dialog';
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
+import ImageListItemBar from '@mui/material/ImageListItemBar';
 
 
 function Basket() {
@@ -17,7 +21,7 @@ function Basket() {
       const handleClose = () => {
         setOpen(false);
       };
-    
+      const [maxWidth, setMaxWidth] = React.useState<DialogProps['maxWidth']>('sm');
     return (
         <React.Fragment>
             <IconButton sx={{marginRight: 1}}
@@ -25,18 +29,49 @@ function Basket() {
                 color='inherit'>
                     <ShoppingBasket />
             </IconButton>
-            <Dialog open={open}>
-                <Box >
+            <Dialog 
+            open={open} 
+            onClose={handleClose}
+            maxWidth={maxWidth}
+            >
+
+                <Box>
                     <Box display="flex" justifyContent="space-evenly">
                     
-                        <Typography variant="h4">Корзина</Typography>
+                        <DialogContentText variant="h4" m={2} >Корзина</DialogContentText>
                         <Chip 
                             icon={<ProductionQuantityLimitsIcon />} 
-                            label='Минимальная сумма заказа 900р' />
+                            label='Минимальная сумма заказа 900р' 
+                            sx={{margin: 3}} />
+                            <IconButton onClick={handleClose} sx={{margin: 1}}>
+                                <CloseIcon />
+                            </IconButton>
                     </Box>
-                    <Divider variant="middle" flexItem sx={{ marginTop: 3, marginBottom: 5,   }} />
+                    <Divider variant="middle" flexItem sx={{ marginTop: 3, marginBottom: 5}} />
                     <Box>
-                        <Typography>Хай</Typography>
+                        <Grid sx={{ flexGrow: 1 }} container justifyContent="center">
+                            <Grid item xs={1}>
+                    
+                                <Grid container justifyContent="center" spacing={2}>
+                                {items.map((items, index) => (
+                                    <Grid key={index} item>
+                                        <CardActionArea>
+                                            <Card style={{ backgroundImage: `url(${items.image})`,
+                                            backgroundSize: 'cover',
+                                            width: 270,
+                                            height: 270,
+                                            color: 'white',
+                                            borderRadius: 13 }}>
+                                            <CardContent>
+                                                <Typography variant="h6">{items.name}</Typography>
+                                            </CardContent>
+                                        </Card>
+                                    </CardActionArea>
+                                    </Grid> 
+                                ))}
+                                </Grid>
+                            </Grid>
+                        </Grid>
                     </Box>
                 </Box>
             </Dialog>
@@ -44,3 +79,37 @@ function Basket() {
     )
 }
 export default Basket;
+
+export const items = [
+    {
+        name: 'lol',
+        image: 'https://cdn.esh-derevenskoe.ru/image/cache/catalog/9b8a49-400x400.jpg?v=3'
+
+    },
+    {
+        name: 'lol2',
+        image: 'https://cdn.esh-derevenskoe.ru/image/cache/catalog/eee36c-400x400.jpg?v=3'
+
+    },
+    {
+        name: 'lol3',
+        image: 'https://cdn.esh-derevenskoe.ru/image/cache/catalog/cccbdf-400x400.jpeg?v=3'
+
+    },
+    {
+        name: 'lol4',
+        image: 'https://cdn.esh-derevenskoe.ru/image/cache/catalog/844a7d-400x400.jpg?v=3'
+
+    },
+    {
+        name: 'lol',
+        image: 'https://cdn.esh-derevenskoe.ru/image/cache/catalog/fdd925-400x400.jpg?v=3'
+
+    },
+    {
+        name: 'lol2',
+        image: 'https://cdn.esh-derevenskoe.ru/image/cache/catalog/eee36c-400x400.jpg?v=3'
+
+    },
+   
+  ];
